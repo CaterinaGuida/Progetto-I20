eea87abc6ea867f5ab74399bdc65de382851bdbf
ceres <francesco.ceresa02@universitadipavia.it>
2020-09-07 16:36:18 +0200

    sistemato

---
+++ src/controller/util/ConversationUtil.java
@@ -0,0 +1,28 @@
+package controller.util;
+
+import java.util.ArrayList;
+
+import solver.Answer;
+
+
+
+public class ConversationUtil {
+	public static ArrayList<String> fromAnswersToStrings(ArrayList<Answer> options){
+		ArrayList<String> strOp = new ArrayList<String>();
+		for(Answer a: options) {
+			strOp.add(a.getText());
+		}
+		
+		return strOp;
+	}
+	
+	public static Answer fromStringToAnswer(String s, ArrayList<Answer> options) {// necessita della lista answer originale
+		
+		for(Answer a: options) {
+			if(a.getText().equals(s))
+				return a;
+		}
+		return null;
+	}
+}
+
---
+++ src/solver/SolverThread.java
@@ -0,0 +1,28 @@
+package solver;
+
+import controller.util.SolverFacade;
+
+public class SolverThread extends Thread{
+	private Conversation conv;
+	private String thrName;
+	
+	public SolverThread(String name) {
+		// TODO Auto-generated constructor stub
+		super();
+		this.thrName=name;
+		
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		conv= new Conversation();
+		System.out.println("Thread " + thrName + "Started Successfully");
+		
+	}
+	
+	public synchronized SolverFacade getSolverIstance() {
+		System.out.println("request on Thread " + thrName + "Started Successfully");
+		return new SolverFacade(conv);
+	}
+
+}
