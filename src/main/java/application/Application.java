84a157fe0b85bc3a2953fba54bddeabfc4cc0ab5
ceres <francesco.ceresa02@universitadipavia.it>
2020-09-09 14:32:44 +0200

    Sistemazione gradle

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,20 +1,19 @@
-package solver.solver;
+package application;
 
 import webui.server.ApplicationServer;
 import webui.server.SessionManager;
 import webui.server.SolverServlet;
 
-public class Test {
+public class Application {
 
 	public static void main(String[] args) {
-		
+		// TODO Auto-generated method stub
 		try {
 			new ApplicationServer(8080,new SolverServlet(new SessionManager())).start();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-
 	}
+
 }
