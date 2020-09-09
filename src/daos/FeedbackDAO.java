b7aa3128c4d4d6cb2e1d975cd8c9547439392f86
Davide Ligari <davide.ligari01@universitadipavia.it>
2020-09-09 10:00:57 +0200

    tolti import innutili classi DAO

--- src/daos/FeedbackDAO.java
+++ src/daos/FeedbackDAO.java
@@ -2,13 +2,12 @@ package daos;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 public class FeedbackDAO {
-	public static void saveFeedback(String description,String contatto,String id_prodotto,Boolean satisfied) { //metodo usato per caricare nel DB il feedback
-		PreparedStatement st1=null;                                                          // attenzione che si inseriscono due feedback 
-		Connection conn=null;																// con la stessa descrizione e contatto genera un eccezione 
-		try {																				// dato che questi due valori sono chiave primaria
+	public static void saveFeedback(String description,String contatto,String id_prodotto,Boolean satisfied) { 
+		PreparedStatement st1=null;                                                           
+		Connection conn=null;												 
+		try {													
 			conn=DBConnection.startConnection(conn);
 			String query="insert into feedback (`contatto`, `description`, `id_product`,`satisfied`)values (?,?,?,?)";
 			st1=conn.prepareStatement(query);
