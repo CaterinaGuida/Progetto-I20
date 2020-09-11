4600aedd46638b5ad2a4f736d28ac37f58ceb35e
ceres <francesco.ceresa02@universitadipavia.it>
2020-09-11 15:56:19 +0200

    Class Fine Tuning

---
+++ src/application/MainClass.java
@@ -0,0 +1,12 @@
+package application;
+
+import webui.WebApplication;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebApplication.getIstance().startWebApplicationServer();
+	}
+
+}
---
+++ src/webui/WebApplication.java
@@ -0,0 +1,41 @@
+package webui;
+
+import webui.server.ApplicationServer;
+import webui.server.SessionManager;
+import webui.server.SolverServlet;
+
+public class WebApplication {	//Singleton consente di attivare solo una istanza del server dell' applicazione
+	private static WebApplication istance = null;
+	private boolean isServerRunning;
+	
+	private WebApplication() {
+		isServerRunning= false;
+	}
+	
+	public static WebApplication getIstance() {
+		if(istance == null)
+			istance = new WebApplication();
+		
+		
+		return istance;
+	}
+	
+	private void serverStart() {
+		try {
+			ApplicationServer app= new ApplicationServer(8080, new SolverServlet(new SessionManager()));
+			app.start();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void startWebApplicationServer() {
+		if(!isServerRunning) {
+			serverStart();
+			isServerRunning=true;
+		}
+	}
+	
+
+}
